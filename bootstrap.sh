fed7d29b9e5288aa6eb8a7968cffc7a053149af9
Serhii <spridoloba@gmail.com>
2025-10-31 00:50:59 +0200

    Solution 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-
-kind create cluster --name todoapp --config cluster.yml
-
-
-kubectl create namespace todoapp
-
-
-helm dependency update .
-
-
-helm install todoapp . --namespace todoapp --create-namespace
-
-
-kubectl get all -n todoapp
+kind create cluster --name todoapp
+kubectl create namespace todoapp --dry-run=client -o yaml | kubectl apply -f -
+helm dependency update ./charts/todoapp
+helm install todoapp ./charts/todoapp -n todoapp --create-namespace
+kubectl get all,cm,secret,ing -A | tee output.log
